e8c82b2c417df545e596488742a6a08c7a0ecb4b
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-15 03:18:31 +0530

    Remove Home component from app/page.tsx and integrate it into app/(dashboard)/page.tsx, enhancing the dashboard with WelcomeMessage and loading state using FallbackWelcomeMessage.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton";
+import WelcomeMessage from "@/components/WelcomeMessage";
+import { Suspense } from "react";
 
-const Dashboard = () => {
-    return (
-        <div>Dashboard</div>
-    )
+export default async function Home() {
+  return (
+    <>
+      <Suspense fallback={<FallbackWelcomeMessage />}>
+        <WelcomeMessage />
+      </Suspense>
+    </>
+  )
 }
 
-export default Dashboard
+import React from 'react'
+
+export const FallbackWelcomeMessage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">
+      <Skeleton className="w-[180px] h-[36px]" />
+      <Skeleton className="w-[180px] h-[36px]" />
+     
+      </h1>
+    </div>
+  )
+}
