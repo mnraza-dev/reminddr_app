0339a9749c03ea605f56d154f6152c5ecb3d5171
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-15 02:03:07 +0530

    update sign-up page to use Clerk's SignUp component.

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,7 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
     return <div className='flex justify-center items-center min-h-screen'>
-
-        <SignUp />
+        <SignIn />
     </div>
 }
