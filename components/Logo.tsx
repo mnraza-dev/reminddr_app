79ea511e91f87f1d532140ea8556977c5f84fec8
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-15 02:20:54 +0530

    Update Logo and Navbar components with improved styling and layout

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <div className=''>
+      <h1 className='text-xl font-semibold bg-gradient-to-r from-red-100 to-orange-500 bg-clip-text text-transparent'>Reminddr</h1>
+
+    </div>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,9 @@ import Logo from './Logo'
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className='flex w-full items-center justify-between p-4 px-8 h-[60px] '>
             <Logo />
-            <div>
+            <div className='flex gap-4 items-center'>
                 <UserButton afterSignOutUrl='/' />
                 <ThemeSwitcher />
 
