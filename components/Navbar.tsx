7da847561e2c526035b91fe1187d6bb492268033
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-15 03:20:22 +0530

    Update Navbar component to use sticky positioning and backdrop blur for improved visibility and aesthetics

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,11 @@ import Logo from './Logo'
 
 export default function Navbar() {
     return (
-        <nav className='flex w-full items-center justify-between p-4 px-8 h-[60px] '>
+        <nav className='sticky top-0 left-0 z-50 backdrop-blur-2xl flex w-full items-center justify-between p-4 px-8 h-[60px] '>
             <Logo />
             <div className='flex gap-4 items-center'>
                 <UserButton afterSignOutUrl='/' />
                 <ThemeSwitcher />
-
             </div>
         </nav>
     )
